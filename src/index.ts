a3c01a88db66c08910ba009f1d95fa56a386394b
Giselle van Dongen <giselle.vd@gmail.com>
2026-01-27 09:24:08 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,18 @@ export {
   getTerminalToolSteps,
   rethrowTerminalToolError,
 } from './lib/ai_infra';
-export { createRestateMCPClient, RestateMCPClient } from './lib/mcp_client';
+export {
+  createRestateMCPClient,
+  RestateMCPClient,
+  type ToolSchemas,
+  type ListResourcesResult,
+  type PaginatedRequest,
+  type RequestOptions,
+  type ReadResourceResult,
+  type ListResourceTemplatesResult,
+  type ListPromptsResult,
+  type GetPromptResult,
+} from './lib/mcp_client';
 export type {
   Context,
   RunOptions,
